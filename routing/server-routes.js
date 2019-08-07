f7b5b12b5a8eb00836e8e602d1ebb020fac9f0a8
Ernesto del Frade <ernesto.frade@m8agency.com>
2019-08-07 14:37:45 -0400

    demo data

--- routing/server-routes.js
+++ routing/server-routes.js
@@ -1,39 +1,71 @@
 const express = require('express');
 const basicAuth = require('express-basic-auth');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const router = express.Router();
 
-router.use(basicAuth({users: { [process.env.BASIC_USER]: process.env.BASIC_PW }}));
+router.use(basicAuth({ users: { [process.env.BASIC_USER]: process.env.BASIC_PW } }));
 
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
+    user(id: Int!): User
+    users(city: String): [User]
+  },
+  type User {
+    id: Int
+    name: String
   }
 `);
 
+const usersData = [
+  {
+    id: 1,
+    name: 'Jason Primer',
+    city: 'New York'
+  },
+  {
+    id: 2,
+    name: 'John Stamos',
+    city: 'New York'
+  },
+  {
+    id: 3,
+    name: 'Peter Parker',
+    city: 'Miami'
+  }
+];
+
+const getUser = (args) => {
+  var id = args.id;
+  return usersData.filter(user => {
+    return user.id == id;
+  })[0];
+}
+
+const getUsers = (args) => {
+  if (args.topic) {
+    var city = args.city;
+    return usersData.filter(user => user.city === city);
+  } else {
+    return usersData;
+  }
+}
+
 // The root provides a resolver function for each API endpoint
-var root = {
-    hello: () => {
-        return 'Hello world!';
-    },
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  user: getUser,
+  users: getUsers
 };
 
-
 router.use('/', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
 }));
-
-
-
-
-
-// router.get('/', (req, res) => {
-//     const page = 'api';
-//     return res.status(200).send(page);
-// });
 
 module.exports = router;
