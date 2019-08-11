ba4b997d948f793107e5b8717d8f4df28ebd01ae
Ernesto del Frade <ernerock6@gmail.com>
2019-08-10 22:46:22 -0400

    laying out page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ScrollableAnchor from "react-scrollable-anchor";
 import Apple from "../components/Apple/";
+import Range from "../components/elements/Range";
 
 const Home = styled.div``;
 
@@ -36,84 +37,178 @@ const PageIntro = styled.div`
 const Page = styled.div`
   height: calc(100vh - 80px);
   background-color: #fff;
+  position: relative;
+  overflow: hidden;
   .heading {
     width: 100%;
-    background-color: #ebebeb;
-    padding: 30px 40px;
+    background: linear-gradient(
+      90deg,
+      rgba(242, 242, 242, 1) 0%,
+      rgba(255, 255, 255, 1) 100%
+    );
+    padding: 80px 90px;
+    width: 50%;
     h3 {
-      font-size: 18px;
-      color: red;
-    }
-    h2 {
-      font-size: 70px;
+      font-size: 32px;
+      line-height: 48px;
+      color: #ff3b30;
       font-weight: bold;
     }
+    h2 {
+      font-size: 100px;
+      line-height: 110px;
+      font-weight: 700;
+      font-family: "Poppins";
+    }
     p {
-      color: #a5a5a5;
+      font-size: 25px;
+      line-height: 35px;
+      color: #999999;
+      max-width: 612px;
     }
   }
   .phones {
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    width: 50%;
     .pricing {
       h4 {
-        color: #a5a5a5;
-        font-size: 25px;
+        color: #cccccc;
+        font-size: 50px;
+        font-family: "Helvetica";
+        font-weight: 700;
       }
       a {
-        color: red;
+        color: #ff3b30;
+        font-size: 20px;
+        font-weight: 600;
       }
     }
     .options {
+      position: relative;
+      max-width: 300px;
+      .variants {
+        img {
+          opacity: 0.2;
+          transition: opacity ease 0.5s;
+          &.active {
+            opacity: 1;
+            transition: opacity ease 0.5s;
+          }
+        }
+      }
+    }
+  }
+  .large-phones {
+    position: absolute;
+    right: 40px;
+    top: 100px;
+    .variants {
+      position: relative;
+      width: 500px;
+      height: 100vh;
+      img {
+        position: absolute;
+        left: 0;
+        top: 0;
+        transform: translateX(200%);
+        opacity: 0;
+        transition: all ease 0.5s;
+        &.active {
+          transform: translateX(0);
+          transition: all ease 0.5s;
+          opacity: 1;
+        }
+      }
     }
   }
 `;
 
-const Homepage = () => (
-  <Home>
-    <PageIntro>
-      <Apple
-        img="/static/images/taller-logo-grey.png"
-        copy="Welcome to Apple"
-        bold
-      />
-      <a className="see-all" href="">
-        See our products
-      </a>
-      <div className="devices">
-        <a href="#phones">
-          <img src="/static/images/phone.png" alt="Phone" />
+const Homepage = () => {
+  const [option, setOption] = useState(0);
+  const okChange = e => {
+    setOption(parseInt(e.target.value));
+    // console.log(e.target.value);
+  };
+  return (
+    <Home>
+      <PageIntro>
+        <Apple
+          img="/static/images/taller-logo-grey.png"
+          copy="Welcome to Apple"
+          bold
+        />
+        <a className="see-all" href="#phones">
+          See our products
         </a>
-        <a href="#phones">
-          <img src="/static/images/mac.png" alt="Laptop" />
-        </a>
-        <a href="#phones">
-          <img src="/static/images/watch.png" alt="Watch" />
-        </a>
-      </div>
-    </PageIntro>
-    <ScrollableAnchor id="phones">
-      <Page>
-        <div className="heading">
-          <h3>iPhone</h3>
-          <h2>
-            The ultimage
-            <br />
-            iPhone
-          </h2>
-          <p>
-            The future is here. Join the iPhone Upgrade Program to get the
-            latest iPhone - NOW
-          </p>
+        <div className="devices">
+          <a href="#phones">
+            <img src="/static/images/phone.png" alt="Phone" />
+          </a>
+          <a href="#phones">
+            <img src="/static/images/mac.png" alt="Laptop" />
+          </a>
+          <a href="#phones">
+            <img src="/static/images/watch.png" alt="Watch" />
+          </a>
         </div>
-        <div className="phones">
-          <div className="pricing">
-            <h4>From $699</h4>
-            <a>Buy Now ></a>
+      </PageIntro>
+      <ScrollableAnchor id="phones">
+        <Page>
+          <div className="heading">
+            <h3>iPhone</h3>
+            <h2>
+              The ultimage
+              <br />
+              iPhone
+            </h2>
+            <p>
+              The future is here. Join the iPhone Upgrade Program to get the
+              latest iPhone - NOW
+            </p>
           </div>
-          <div className="options" />
-        </div>
-      </Page>
-    </ScrollableAnchor>
-  </Home>
-);
+          <div className="phones">
+            <div className="pricing">
+              <h4>From $699</h4>
+              <a>Buy Now ></a>
+            </div>
+            <div className="options">
+              <div className="variants">
+                <img
+                  className={option === 0 ? "active" : ""}
+                  src="/static/images/iphone.png"
+                  alt=""
+                />
+                <img
+                  className={option === 1 ? "active" : ""}
+                  src="/static/images/iphone-back.png"
+                  alt=""
+                />
+              </div>
+              <div className="slider">
+                <Range min="0" max="1" value={option} onChange={okChange} />
+              </div>
+            </div>
+          </div>
+          <div className="large-phones">
+            <div className="variants">
+              <img
+                className={option === 0 ? "active" : ""}
+                src="/static/images/iphone-large.png"
+                alt=""
+              />
+              <img
+                className={option === 1 ? "active" : ""}
+                src="/static/images/iphone-back-large.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </Page>
+      </ScrollableAnchor>
+    </Home>
+  );
+};
 
 export default Homepage;
